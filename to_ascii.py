e31ca288b85e6d37f1b1e68958ff874f84009547
whabbot <61473790+whabbot@users.noreply.github.com>
2020-08-05 06:12:58 -1100

    Add unit tests for to_ascii

--- to_ascii.py
+++ to_ascii.py
@@ -3,13 +3,14 @@
 Functions:
     brightnesses_to_asciis(list) -> list. Convert list of brightness
         values to corresponding ASCII characters.
-    img_to_ascii(Image object/2D Numpy array, int/float) -> (tuple, 2D map object).
-        Convert Image object or Numpy array representing an image into
-        2D map object of ASCII characters.
+    img_to_ascii(Image object/3D Numpy array, int/float) -> (tuple, map object).
+        Convert Image object or RGB pixel Numpy array into map object
+        of 2D ASCII character array.
     text_image(2D sequence) -> Image object. Draws ASCII characters
         onto Image object.
 """
 
+import sys
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
@@ -20,8 +21,8 @@ def brightnesses_to_asciis(brightness_list):
     Parameters:
         brightness_list: list. Pixel brightnesses for row of image.
     Returns:
-        Map object. The corresponding ASCII characters for each
-            brightness.
+        Map object. 1D array of the corresponding ASCII characters for
+        each brightness.
     """
     ascii = {0: '`', 1: '^', 2: '"', 3: ',', 4: ':', 5: ';', 6: 'I',
         7: 'l', 8: '!', 9: 'i', 10: '~', 11: '+', 12: '_', 13: '-',
@@ -38,19 +39,22 @@ def brightnesses_to_asciis(brightness_list):
 
 
 def img_to_ascii(img):
-    """Convert Image object or Numpy array representing an image into
-    2D map object of ASCII characters.
+    """Convert Image object or RGB pixel Numpy array into map object of
+    2D ASCII character array.
 
     Parameters:
-        img: PIL Image object or Numpy array representing an image.
+        img: PIL Image object or RGB pixel Numpy array.
     Returns:
-        Map object. ASCII characters representing each pixel of
-            original image.
+        Map object. 2D array of ASCII characters representing each
+            pixel of original image.
         Tuple. Shape of the image in pixels (height, width).
     """
     # Convert image to 2d Numpy array of rgb values.
     if type(img) != np.ndarray:
         img = np.array(img)
+    if np.ndim(img) != 3 or len(img[0][0]) != 3:
+        raise ValueError('Expect data as 3D array of RGB data.')
+   
     # Use average of RGB as measure for brightness.
     brightnesses = np.average(img, axis=2)
 
@@ -62,10 +66,10 @@ def img_to_ascii(img):
 
 
 def text_image(ascii_characters, image_shape):
-    """Draw 2D sequence of ASCII characters onto PIL Image.
+    """Draw 2D array of ASCII characters onto PIL Image.
 
     Parameters:
-        ascii_characters: 2D sequence. ASCII characters for drawing
+        ascii_characters: 2D array. ASCII characters for drawing
             onto image.
             [
                 [row1],
@@ -79,7 +83,7 @@ def text_image(ascii_characters, image_shape):
     text = '\n'.join(''.join(row) for row in ascii_characters)
 
     # Size of the text in pixels: tuple (width, height).
-    default_font = ImageDraw.Draw(Image.new('RGBA', (1, 1), (0, 0, 0, 1))).getfont()
+    default_font = ImageDraw.Draw(Image.new('RGB', (1, 1), (0, 0, 0))).getfont()
     text_width, text_height = default_font.getsize(text)
 
     # Calculate size of canvas to draw text onto.
@@ -87,7 +91,7 @@ def text_image(ascii_characters, image_shape):
     canvas_height = int(num_lines * text_height - num_lines)
     canvas_width = int(text_width / num_lines)
 
-    # Create the canvas for the final picture
+    # Create canvas and draw text.
     textimage = Image.new('RGB', (canvas_width, canvas_height), (0, 0, 0))
     text_draw = ImageDraw.Draw(textimage)
     text_draw.multiline_text((0,0), text, spacing=-1)
